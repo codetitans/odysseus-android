2c963554f531d2e5c8d47f7fe569fd5905109eda
Paweł Hofman <pawel@codetitans.pl>
2026-07-27 02:13:58 +0200

    Added publishing to GitHub

--- src/odysseus/build.gradle.kts
+++ src/odysseus/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("maven-publish")
 }
 
 android {
@@ -15,11 +16,32 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = false
+            // proguardFiles getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro"
+            enableUnitTestCoverage = false
+            enableAndroidTestCoverage = false
+        }
+        debug {
+            isMinifyEnabled = false
+            enableUnitTestCoverage = true
+            enableAndroidTestCoverage = true
+        }
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -28,4 +50,33 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+}
+
+// Published as pl.codetitans:odysseus:<version> to GitHub Packages.
+// AGP only registers the "release" software component after the project has been evaluated,
+// so the publication/repository setup has to be wired up in afterEvaluate.
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "pl.codetitans"
+                artifactId = "odysseus"
+                version = project.findProperty("odysseusVersion") as String? ?: "0.0.1"
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/codetitans/odysseus-android")
+
+                credentials {
+                    username = System.getenv("GITHUB_USERNAME") ?: project.findProperty("github.username") as String?
+                    password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("github.token") as String?
+                }
+            }
+        }
+    }
 }
