b18263c52143307e3bb54d7abb7d9c2382d05649
Paweł Hofman <pawel@codetitans.pl>
2026-08-13 02:12:45 +0200

    Bumped version

--- publish-bundle.sh
+++ publish-bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=${1:-0.0.9}
+version=${1:-0.0.10}
 bundle_name=odysseus-$version-bundle.zip
 bundle_path=bundle/pl/codetitans/odysseus/
 repo_path=~/.m2/repository/pl/codetitans/odysseus/$version/
